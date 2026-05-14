f7ed255fa9ffe6adb3bfba10cfc1f302c495661e
AMANITE9 <sacha.guillemin1@gmail.com>
2026-05-14 21:03:39 +0200

    fix web structure

---
+++ script.js
@@ -0,0 +1,99 @@
+const audio = new Audio();
+
+const bubbles =
+document.querySelectorAll('.demo-bubble');
+
+const progressBar =
+document.querySelector('.progress-fill');
+
+const progressContainers =
+document.querySelectorAll('.progress-bar');
+
+let currentBubble = null;
+
+/* ================= PLAY / PAUSE ================= */
+
+bubbles.forEach(bubble => {
+
+    bubble.addEventListener('click', () => {
+
+        const track =
+        bubble.dataset.track;
+
+        /* MEME PISTE = PAUSE */
+
+        if(audio.src.includes(track)
+        && !audio.paused){
+
+            audio.pause();
+
+            bubble.classList.remove('playing');
+
+            return;
+        }
+
+        /* RESET AUTRES BULLES */
+
+        bubbles.forEach(b =>
+
+            b.classList.remove('playing')
+        );
+
+        /* NOUVELLE PISTE */
+
+        audio.src = track;
+
+        audio.play();
+
+        bubble.classList.add('playing');
+
+        currentBubble = bubble;
+    });
+});
+
+/* ================= UPDATE PROGRESSION ================= */
+
+audio.addEventListener('timeupdate', () => {
+
+    if(!audio.duration) return;
+
+    const progress =
+
+    (audio.currentTime / audio.duration) * 100;
+
+    progressBar.style.width =
+    progress + '%';
+});
+
+/* ================= CLIQUER BARRE ================= */
+
+progressContainers.forEach(bar => {
+
+    bar.addEventListener('click', (e) => {
+
+        const width =
+        bar.clientWidth;
+
+        const clickX =
+        e.offsetX;
+
+        const duration =
+        audio.duration;
+
+        audio.currentTime =
+        (clickX / width) * duration;
+    });
+
+});
+
+/* ================= FIN MUSIQUE ================= */
+
+audio.addEventListener('ended', () => {
+
+    if(currentBubble){
+
+        currentBubble.classList.remove('playing');
+    }
+
+    progressBar.style.width = '0%';
+});
